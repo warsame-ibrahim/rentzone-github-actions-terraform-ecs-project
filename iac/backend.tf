81f1e05faaa66b11efd5e89c3787cc0078f75896
Warsame <Warsame.mxi@hotmail.com>
2023-10-14 13:01:05 +0100

    updated code

--- iac/backend.tf
+++ iac/backend.tf
@@ -1,9 +1,9 @@
 # store the terraform state file in s3 and lock with dynamodb
 terraform {
   backend "s3" {
-    bucket         = 
-    key            = 
-    region         = 
-    dynamodb_table = 
+    bucket         = "warsame-terraform-remote-state"
+    key            = "rentzone-app/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-lock"
   }
 }
